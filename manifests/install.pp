bdded94a747a25e9100402e65178963c03ce319b
root <root@pmontesd1.mylabserver.com>
2017-09-24 10:19:27 +0000

    Update install, config and service

---
+++ manifests/install.pp
@@ -0,0 +1,11 @@
+class nginx::install (
+  String $ensure       = $::nginx::ensure,
+  String $package_name = $::nginx::package_name,
+) {
+
+  package { 'install_ngnix':
+    ensure => $ensure,
+    name   => $package_name,
+  }
+
+}
